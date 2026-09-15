1b2f4d038be6cb6ffcdb6b0bf790e3d1e87a42af
Manuel Achterberg <manuel.achterberg@mailbox.org>
2026-09-15 13:35:34 +0200

    feat: enable sunrays for homepage fluid

--- fluid-background.js
+++ fluid-background.js
@@ -33,7 +33,7 @@ if (host && !prefersReducedMotion) {
       transparent: true,
       brightness: 0.28,
       bloom: false,
-      sunrays: false
+      sunrays: true
     });
 
     fluid.start();
